bb00a7cb94bea2fd2cd6551d9887a5ba39dd6082
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-15 13:02:44 +0200

    [UPD] Forbidden item response added.

--- 03-deploying-ml-pipelines/03-api-deployment/01-parameters/main.py
+++ 03-deploying-ml-pipelines/03-api-deployment/01-parameters/main.py
@@ -6,14 +6,20 @@ Created on: 4/14/2022
 Licence,
 """
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
 
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
+
+
+class Message(BaseModel):
+    message: str
 
 
 app = FastAPI()
@@ -25,7 +31,14 @@ async def create_item(
     return item
 
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", responses={404: {"model": Message}})
 async def order_item(
-        item_id: int, quantity: int):
+        item_id: int,
+        quantity: int
+):
+    if item_id == 27:
+        return JSONResponse(
+            status_code=404,
+            content={"message": "Forbidden Item."})
+
     return {'item_id': item_id, 'quantity': quantity}
