6768f37b369608c2f094ffe7931d4a1e3be8f8c4
Heber Trujillo <heber.trj.urt@gmail.com>
2022-04-30 19:23:32 +0200

    [NEW] Python example for cron automation.

---
+++ 04-ml-model-scoring-monitoring/01-model-training-deployment/01-cron-automation/write_file.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""This script writes a file
+
+Created on: 4/30/2022
+@author: Heber Trujillo <heber.trj.urt@gmail.com> 
+Licence,
+"""
+
+with open("./file.txt", "w") as file:
+    file.write("This is a file generates with cron")
